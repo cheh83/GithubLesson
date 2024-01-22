73f1a2cb17fbe39a428556512039ad1eb51d0eaf
Oleg <Chehchekhovsky@gmail.com>
2024-01-22 18:41:11 +0200

    delete

--- func.py
+++ func.py
@@ -3,13 +3,13 @@ class Dog:
     age = None
     isHappy = True
 
-dog1 = Dog() # это экземпляр класса
+dog1 = Dog() 
 dog1.name = 'Alfa'
 dog1.age = 2
 dog1.isHappy = True
 print('name: ', dog1.name, 'age:', dog1.age, dog1.isHappy)
 
-dog2 = Dog() # это экземпляр класса
+dog2 = Dog() 
 dog2.name = 'Bob'
 dog2.age = 5
 dog2.isHappy = False
